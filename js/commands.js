3eb02671099df74cffdcf78e58f108adc276ca84
slvr <info@uxuid.co>
2015-03-10 15:00:03 -0700

    Agrego los comandos de consola a commands.js

--- js/commands.js
+++ js/commands.js
@@ -1,7 +1,7 @@
 var commands = {
 	'': '',
 	'clear': 'Limpiar',
-	'help': 'Comandos disponibles:<br />clear<br />help<br />url<br /><br />alert',
+	'help': 'Comandos disponibles:<br />clear<br />1. Info<br />2. Estudios<br />3. Habilidades<br />4. Trabajos<br />5. Proyectos<br />6. contacto',
 	'url': 'https://desarrolloactivo.com/',
 	"alert": 'alert("función alert");'
 }
