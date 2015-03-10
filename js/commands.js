b5e7de3d9a97d7d03693a36498bbca413bbc1b7d
Esteban Fuster Pozzi <estebanrfp@gmail.com>
2015-03-11 00:15:30 +0100

    comandos resumidos

--- js/commands.js
+++ js/commands.js
@@ -1,6 +1,11 @@
 var commands = {
 	'': '',
 	'clear': 'Limpiar',
-	'help': 'Comandos disponibles:<br />clear<br />info<br />estudios<br />habilidades<br />trabajos<br />proyectos<br />contacto',
-	'info': '<p>Hola, soy @estebanrfp, Fullstack developer, entusiasta de la tecnología web.<span>&nbsp;</span></p>'
+	'help': 'Comandos disponibles:<br />clear<br />info [i]<br />estudios [es]<br />habilidades [hab]<br />trabajos [tr]<br />proyectos [py]<br />contacto [cnt]',
+	'i': '<p>Hola, soy @estebanrfp, Fullstack developer, entusiasta de la tecnología web.<span>&nbsp;</span></p>',
+	'es': '<p>estudios.<span>&nbsp;</span></p>',
+	'hab': '<p>habilidades.<span>&nbsp;</span></p>',
+	'tr': '<p>trabajos.<span>&nbsp;</span></p>',
+	'py': '<p>proyectos.<span>&nbsp;</span></p>',
+	'cnt': '<p>contacto.<span>&nbsp;</span></p>'
 }
