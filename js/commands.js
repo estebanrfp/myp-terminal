b8673efc3d490f38ca389c20c363bd4f8b7ef33a
slvr <info@uxuid.co>
2015-03-10 15:07:16 -0700

    mayuscula commands.js

--- js/commands.js
+++ js/commands.js
@@ -1,7 +1,7 @@
 var commands = {
 	'': '',
 	'clear': 'Limpiar',
-	'help': 'Comandos disponibles:<br />clear<br />Info<br />estudios<br />habilidades<br />trabajos<br />proyectos<br />contacto',
+	'help': 'Comandos disponibles:<br />clear<br />info<br />estudios<br />habilidades<br />trabajos<br />proyectos<br />contacto',
 	'url': 'https://desarrolloactivo.com/',
 	"alert": 'alert("función alert");'
 }
