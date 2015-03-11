63229ebfd5e2a7d9dc9bc517387cea7fc67207b7
Esteban Fuster Pozzi <estebanrfp@gmail.com>
2015-03-11 03:53:02 +0100

    arreglo un comando en commands.js

--- js/commands.js
+++ js/commands.js
@@ -2,7 +2,7 @@ var commands = {
 	'': '',
 	'clear': 'Limpiar',
 	'help': 'Comandos disponibles:<br />clear<br />info [i]<br />estudios [es]<br />habilidades [hab]<br />trabajos [tr]<br />proyectos [py]<br />contacto [cnt]',
-	'i': '<p>Hola, soy @estebanrfp, Fullstack developer, entusiasta de la tecnología web.<span>&nbsp;</span></p>',
+	'i': 'Hola, soy @estebanrfp, Fullstack developer, entusiasta de la tecnología web.',
 	'es': 'estudios.',
 	'hab': 'habilidades.',
 	'tr': 'trabajos.',
