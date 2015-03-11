392a6daec86ab3df9ff9ff7e474c452dbdb2dca5
Esteban Fuster Pozzi <estebanrfp@gmail.com>
2015-03-12 00:18:31 +0100

    agregamos información a commands.js

--- js/commands.js
+++ js/commands.js
@@ -3,8 +3,8 @@ var commands = {
 	'clear': 'Limpiar',
 	'help': 'Comandos disponibles:<br />clear<br />info [i]<br />estudios [es]<br />habilidades [hab]<br />trabajos [tr]<br />proyectos [py]<br />contacto [cnt]',
 	'i': 'Hola, soy @estebanrfp, Fullstack developer, entusiasta de la tecnología web.',
-	'es': 'estudios.',
-	'hab': 'habilidades.',
+	'es': 'estudios cursados.',
+	'hab': 'habilidades, frontend, backend.',
 	'tr': 'trabajos.',
 	'py': 'proyectos.',
 	'cnt': 'contacto.'
