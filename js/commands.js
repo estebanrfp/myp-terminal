a5bae361b0b9652e691d383864020e062bebcb6a
Esteban Fuster Pozzi <estebanrfp@gmail.com>
2015-03-12 23:44:17 +0100

    Actualizamos commands.js poniendo en mayuscula las descripciones.

--- js/commands.js
+++ js/commands.js
@@ -3,9 +3,9 @@ var commands = {
 	'clear': 'Limpiar',
 	'help': 'Comandos disponibles:<br />clear<br />info [i]<br />estudios [es]<br />habilidades [hab]<br />trabajos [tr]<br />proyectos [py]<br />contacto [cnt]',
 	'i': 'Hola, soy @estebanrfp, Fullstack developer, entusiasta de la tecnología web.',
-	'es': 'estudios cursados.',
-	'hab': 'habilidades, frontend, backend.',
-	'tr': 'mis trabajos realizados.',
-	'py': 'mis proyectos.',
-	'cnt': 'mi información de contacto.'
+	'es': 'Estudios cursados.',
+	'hab': 'Habilidades, Frontend, Backend.',
+	'tr': 'Mis trabajos realizados.',
+	'py': 'Mis proyectos.',
+	'cnt': 'Mi información de contacto.'
 }
